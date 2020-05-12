3c682e615d7dbfc8c6c10fa7af6677cfb41a5b8b
Mario Morales <mmario_93@hotmail.es>
2020-05-12 14:42:42 -0600

    cambios

--- server/src/routes/publicacionRoutes.ts
+++ server/src/routes/publicacionRoutes.ts
@@ -11,7 +11,7 @@ class UsuarioRoutes {
     }
 
     config() {
-        this.router.post('/', publicacionController.create);
+        this.router.post('/' , publicacionController.create);
         this.router.get('/:id', publicacionController.traducirPublicacion);
         this.router.get('/mis/:id', publicacionController.todasPublicaciones);
         
